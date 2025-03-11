213053f8fac68de231811e90ae31f8e20e19af3e
leonardolinsz <leonardo.furlanis@germinare.org.br>
2025-03-11 18:09:42 -0300

    Adicionando estrutura swagger

---
+++ WebCotaSpringBoot/src/test/java/org/example/webcotaspringboot/WebCotaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webcotaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCotaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
